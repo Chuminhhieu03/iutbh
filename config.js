01cb5498d2a92a103f8bf9d15c6cb2490949d9fd
chuhieu03 <chuhieuqb2003@gmail.com>
2024-03-08 22:34:00 +0700

    iuiu

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = "Em";
+const giftUrl = "https://www.facebook.com/tran.huyen.1404";
+const eventName = "Chúc Mừng 8-3";
+const titleCard = "Tặng chít chít của anh ";
+const contentCard =
+  "Chúc bé bác sĩ của anh 8/3 vui vẻ nè, luôn luôn xinh đẹp, học giỏi nữa nha, anh yêu em nhiều lắm moaz moaz moaz";
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = "hot-girl.png";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
